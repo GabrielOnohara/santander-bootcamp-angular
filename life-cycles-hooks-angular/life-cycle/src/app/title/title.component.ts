1ba47fe8401dd079b0c609af39649b3204cacf52
GabrielOnohara <gabakio11@gmail.com>
2023-09-20 21:15:22 -0300

    explorando OnInit lc hook

--- life-cycles-hooks-angular/life-cycle/src/app/title/title.component.ts
+++ life-cycles-hooks-angular/life-cycle/src/app/title/title.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
 
+  nome:string = 'felipe'
+
   constructor() { }
 
   ngOnInit(): void {
+    this.nome = `Ola ${this.nome}`
   }
 
 }
