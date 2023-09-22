97c4fdd75c5ca6ba7a6914fcd31310d518cb94c6
GabrielOnohara <gabakio11@gmail.com>
2023-09-21 21:52:55 -0300

    usando ngswitch

--- single-page-application/diretivas-proj/src/app/card/card.component.ts
+++ single-page-application/diretivas-proj/src/app/card/card.component.ts
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
   produtos:string[] = []
-
+  userType:string = ''
   constructor() {
     this.produtos = ["mouse", "teclado", "calculadora"]
+    this.userType = 'admin'
   }
 
   ngOnInit(): void {
